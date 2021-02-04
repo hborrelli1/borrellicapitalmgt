24f92d9ca951be1185288db7674aab057e182d16
Harry Borrelli <hborrelli@Harrys-MacBook-Pro-2.local>
2021-02-03 23:03:13 -0700

    Setup temporary landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+import phone from './Phone.svg';
+import mail from './Mail.svg';
+import linkedin from './LinkedIn.svg';
+import logo from './BCM-Logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Under construction.... Check back in later!</p>
+        <img className="logo" src={logo} alt="Borrelli Capital Management" />
+        <h1>Were doing some maintenance right now... <br/>Check back later!</h1>
+        <div className="divider"></div>
+        <div id="About">
+          <h2>Marcelo Borrelli</h2>
+          <div className="contact-links">
+            <a href="tel:3057231684"><img src={phone} alt="Phone" /></a>
+            <a href="mailto:marcelo.borrelli@insigneo.com"><img src={mail} alt="Email" /></a>
+            <a href="https://www.linkedin.com/in/marcelo-b-7a771a121/" target="_blank"><img src={linkedin} alt="LinkedIn" /></a>
+          </div>
+        </div>
       </header>
     </div>
   );
