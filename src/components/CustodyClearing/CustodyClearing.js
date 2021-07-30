d3f0852b6470b88907c01e7142c398d4a8dc6f87
Harry Borrelli <hborrelli@Harrys-MacBook-Pro-2.local>
2021-07-30 16:12:00 -0600

    Modify sentence.

--- src/components/CustodyClearing/CustodyClearing.js
+++ src/components/CustodyClearing/CustodyClearing.js
@@ -6,9 +6,7 @@ const CustodyClearing = () => {
           <h2 className="section-title">Custody and Clearing</h2>
         </div>
         <div className="info">
-          <p>Your investments are held in a segregated account at Pershing, a subsidiary of Bank of New York Mellon's Corporation. Our advisory accounts are kept at Pershing or other global custodians.
-          <br/>  
-          Although this does not protect you from market-related losses, you can rest assured that in extreme cases, where a financial firm’s integrity is compromised, your assets are fully protected.</p>
+          <p>Your investments are held in a segregated account at Pershing, a subsidiary of Bank of New York Mellon's Corporation. Although this does not protect you from market-related losses, you can rest assured that in extreme cases, where a financial firm’s integrity is compromised, your assets are fully protected.</p>
           <a href="https://www.pershing.com/_global-assets/pdf/pershing-at-a-glance.pdf" target="_blank">Pershing at a Glance</a>
           <a href="https://www.pershing.com/_global-assets/pdf/understanding-the-protection-of-client-assets.pdf" target="_blank">Pershing: Strength, Stability and Focus</a>
         </div>
