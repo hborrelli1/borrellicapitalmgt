b708ac4d75b118665b7b65b5c7c5f67a242262ad
Harry Borrelli <hborrelli@Harrys-MacBook-Pro-2.local>
2021-07-30 17:53:51 -0600

    Update bio text styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,7 +15,8 @@ const About = () => {
           <h4 className="title">Managing Director</h4>
           <h4 className="title">International Financial Advisor</h4>
           <p>Marcelo Borrelli has lived in Guatemala, Colombia, Dominican Republic, Argentina, Minnesota, New Mexico, and Texas before moving to Miami, Florida where he began his financial wealth management career at Morgan Stanley Dean Witter in 1999. After working in the financial services industry for 21 years, Mr. Borrelli decided to go independent and work under the name Borrelli Capital Managament. (***)</p>
-          <p>Specialty Area<br/>
+          <p><span className="special">Specialty Area</span>
+          <br/>
           Structure and tailor investment portfolios for U.S. and Non-U.S. clients.</p>
           <AnchorLink href="#Contact" offset='80'>Contact</AnchorLink>
         </div>
